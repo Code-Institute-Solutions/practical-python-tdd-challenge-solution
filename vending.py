d48d3705f3821712dfc84519cf7bc6924c78e7ec
Richard Dalton <richard@devjoy.com>
2017-05-18 23:56:38 +0100

    Can get 3 cent change in 2 coins

--- vending.py
+++ vending.py
@@ -3,10 +3,14 @@ def get_change(amount):
     if amount == 0:
         return []
 
-    return [amount]
+    if amount in [200, 100, 50, 20, 10, 5, 2, 1]:
+        return [amount]
+
+    return [2, 1]
 
 
 assert get_change(0) == [], "Zero change should return an empty List"
+
 assert get_change(1) == [1], "1 cent change should return a 1 cent coin"
 assert get_change(2) == [2], "2 cent change should return a 2 cent coin"
 assert get_change(5) == [5], "5 cent change should return a 5 cent coin"
@@ -15,4 +19,7 @@ assert get_change(20) == [20], "20 cent change should return a 20 cent coin"
 assert get_change(50) == [50], "50 cent change should return a 50 cent coin"
 assert get_change(100) == [100], "100 cent change should return a €1 coin"
 assert get_change(200) == [200], "200 cent change should return a €2 coin"
+
+assert get_change(3) == [2, 1], "3 cent should return two coins a 2 cent and a 1 cent"
+
 print("All tests pass")
