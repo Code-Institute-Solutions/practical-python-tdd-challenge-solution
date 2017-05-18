be4a0fced69b73ced3f889b98e812b15164ed2bf
Richard Dalton <richard@devjoy.com>
2017-05-18 23:45:45 +0100

    Can get 1 cent change

--- vending.py
+++ vending.py
@@ -1,7 +1,11 @@
 
 def get_change(amount):
-    return []
+    if amount == 0:
+        return []
 
+    return [1]
 
 
 assert get_change(0) == [], "Zero change should return an empty List"
+assert get_change(1) == [1], "1 cent change should return a 1 cent coin"
+print("All tests pass")
